2f5a4a8ac0162b8828796d34febb1b85aa229e4b
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2017-12-26 02:09:11 -0500

    Get all and save them

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+require 'json'
 require "#{Dir.pwd}/lib/raw_request.rb"
 class CryptoExchange
   extend RawRequest
@@ -13,4 +15,16 @@ class CryptoExchange
   def self.find_by_name(name)
     get_request("#{@@url}/#{name}/")
   end
+
+  def self.get_all_and_save(path: "#{Dir.pwd}/files/")
+    data = get_request(@@url)
+    File.open("#{Dir.pwd}/files/data.json", "w") { |file| file.write(data); file.close() }
+    puts "File has been saved".green
+    rescue => exception
+      puts "Error to save".red
+      puts exception.red
+  end
 end
+
+
+puts CryptoExchange.get_all_and_save
